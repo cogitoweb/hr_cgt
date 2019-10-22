79be22935c248f2aa12218d476c8bf49b7d7c6c4
fabiomix <fabiomix@users.noreply.github.com>
2019-10-22 22:26:04 +0200

    kanban for document folders

--- models/hr_document_type.py
+++ models/hr_document_type.py
@@ -11,6 +11,21 @@ class HrDocumentType(models.Model):
     _order = 'sequence, name'
 
 
+    # Compute and search fields
+
+    def _compute_documents_number(self):
+        Documents = self.env['hr.documents']
+
+        for record in self:
+            docs = Documents.search([('document_type', '=', record.id)])
+            # tutti i documenti
+            record.documents_number = len(docs)
+            # documenti con una data di scadenza minore della soglia impostata
+            record.warning_number = len(docs.filtered(lambda x: x.days_left > 0 and x.days_left < x.default_warning_limit_date_hr))
+            # documenti con una data di scadenza superata
+            record.alert_number = len(docs.filtered(lambda x: x.days_left == 0))
+
+
     # Fields declaration
 
     name = fields.Char(
@@ -23,7 +38,40 @@ class HrDocumentType(models.Model):
         default=15
     )
 
+    documents_number = fields.Integer(
+        string='Number of documents',
+        compute=_compute_documents_number,
+        readonly=True
+    )
+
+    warning_number = fields.Integer(
+        string='Number of warnings',
+        compute=_compute_documents_number,
+        readonly=True
+    )
+
+    alert_number = fields.Integer(
+        string='Number of alerts',
+        compute=_compute_documents_number,
+        readonly=True
+    )
+
     sequence = fields.Integer(
         string='Sequence',
         default=20
     )
+
+
+    # Action methods
+
+    @api.multi
+    def action_show_folder_documents(self):
+        action_id = self.env.ref('hr_cgt.view_hr_documents_action').read()[0]
+        # ctx = action_id['context'].copy()
+
+        ctx = {
+            'search_default_document_type': self.id
+        }
+
+        action_id['context'] = ctx
+        return action_id
